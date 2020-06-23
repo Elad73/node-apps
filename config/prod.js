95165f2a4acdf2584caa297db80e349c6ad31305
EladRon <elad.ron.g@gmail.com>
2020-06-23 12:42:56 +0300

    connecting to mapBox api

--- config/prod.js
+++ config/prod.js
@@ -2,6 +2,7 @@
 //prod.js - production keys here!!!
 module.exports = {
     weatherStackKey: process.env.WEATHERSTACK_KEY,
+    mapBoxKey: process.env.MAPBOX_KEY,
     redirectDomain: process.env.REDIRECT_DOMAIN
   };
   
--- weather-app/index.js
+++ weather-app/index.js
@@ -3,10 +3,19 @@ const keys = require('../config/keys');
 
 const globals = require('../globals');
 
-const url = "http://api.weatherstack.com/current?access_key=" + keys.weatherStackKey + "&query=Tel Aviv";
+const urlWeather = "http://api.weatherstack.com/current?access_key=" + keys.weatherStackKey + "&query=Tel Aviv";
 
-request({ url: url, json: true } , (error, response)=> {
-    globals.log("It is currently " + globals.warning(response.body.current.temperature) + " degress out.");
-    globals.log("The weather is " + globals.warning(response.body.current.weather_descriptions));
-    globals.log("It feels like " + globals.warning(response.body.current.feelslike) + " degrees out there!");
+// request({ url: urlWeather, json: true } , (error, response)=> {
+//     globals.log("It is currently " + globals.warning(response.body.current.temperature) + " degress out.");
+//     globals.log("The weather is " + globals.warning(response.body.current.weather_descriptions[0]) + ".");
+//     globals.log("It feels like " + globals.warning(response.body.current.feelslike) + " degrees out there!");
+// });
+
+const urlGeo = "https://api.mapbox.com/geocoding/v5/mapbox.places/Tel Aviv.json?limit=1&access_token=" + keys.mapBoxKey;
+request({url: urlGeo, json:true}, (error, response) => {
+    globals.log(globals.warning(response.body.features[0].place_name));
+    const latitude = response.body.features[0].center[1];
+    const longitude = response.body.features[0].center[0];
+    globals.log("Location: latitude - " + globals.warning(latitude) + ", longitude - " + globals.warning(longitude) + ".");
+
 });
