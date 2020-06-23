c61db56f0770f9fdc9477e08a89e6add739ba1c0
EladRon <elad.ron.g@gmail.com>
2020-06-23 15:07:31 +0300

    refactoring the forcast utils - overloading functions

--- weather-app/utils/forcast.js
+++ weather-app/utils/forcast.js
@@ -3,10 +3,7 @@ const keys = require('../../config/keys');
 
 const serviceUrl = "http://api.weatherstack.com/current?access_key=" + keys.weatherStackKey;
 
-//ToDo: improve the error handling without hard code
-const forcastByCity = (city, callback) => {
-    const url = serviceUrl + "&query=" + city;
-
+const forcastRequest = (url, callback) => {
     request({url: url, json:true}, (error, response) => {
         if(error) {
             callback("Unable to connect to weather services!", undefined);
@@ -23,23 +20,15 @@ const forcastByCity = (city, callback) => {
     });
 }
 
+//ToDo: improve the error handling without hard code
+const forcastByCity = (city, callback) => {
+    const url = serviceUrl + "&query=" + city;
+    forcastRequest(url, callback);
+}
+
 const forcastByCoord = (latitude, longitude, callback) => {
     const url = serviceUrl + "&query=" + latitude + "," + longitude;
-
-    request({url: url, json:true}, (error, response) => {
-        if(error) {
-            callback("Unable to connect to weather services!", undefined);
-        } else if(response.body.error) {
-            callback("Unable to find location!", undefined);
-        }
-        else {
-            callback(undefined, {
-                temperature: response.body.current.temperature,
-                desc: response.body.current.weather_descriptions[0],
-                feelslike: response.body.current.feelslike
-            });
-        }
-    });
+    forcastRequest(url, callback);
 }
 
 module.exports = {forcastByCity, forcastByCoord};
