d80ecbca0db1087d232c6eade63f6c2b0e4f8b62
EladRon <elad.ron.g@gmail.com>
2020-06-23 11:10:47 +0300

    parsing the response from weatherstack

--- weather-app/index.js
+++ weather-app/index.js
@@ -5,9 +5,8 @@ const globals = require('../globals');
 
 const url = "http://api.weatherstack.com/current?access_key=" + keys.weatherStackKey + "&query=Tel Aviv";
 
-request({ url: url } , (error, response)=> {
-    //globals.log(response);
-    const data = JSON.parse(response.body);
-    globals.log(data.current);
-    globals.log(data.location);
+request({ url: url, json: true } , (error, response)=> {
+    globals.log("It is currently " + globals.warning(response.body.current.temperature) + " degress out.");
+    globals.log("The weather is " + globals.warning(response.body.current.weather_descriptions));
+    globals.log("It feels like " + globals.warning(response.body.current.feelslike) + " degrees out there!");
 });
