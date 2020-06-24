779fbebda1a7a8b1c65ee07c4d8c6f09b916a622
EladRon <elad.ron.g@gmail.com>
2020-06-24 09:10:08 +0300

    adding hbs - easy handlebars template engine

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -5,7 +5,31 @@ const keys = require('../../config/keys');
 const app = express();
 const publicDirPath = path.join(__dirname, '../public');
 
+app.set('view engine', 'hbs');
 app.use(express.static(publicDirPath));
+
+app.get('', (req, res) => {
+    res.render('index', {
+        title: "Weather",
+        name: 'Elad Ron'
+    });
+});
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: "About me",
+        name: 'Elad Ron'
+    });
+});
+
+app.get('/help', (req, res) => {
+    res.render('help', {
+        title: "Help page",
+        msg: "How can I assist?",
+        name: 'Elad Ron'
+    });
+});
+
 
 app.get('/weather', (req, res)=>{
     res.send({
