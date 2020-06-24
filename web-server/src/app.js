6c1fdeea1d88216cd040b276251019209432fdb4
EladRon <elad.ron.g@gmail.com>
2020-06-24 10:25:42 +0300

    adding 404 page for the app

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -41,12 +41,29 @@ app.get('/help', (req, res) => {
     });
 });
 
+app.get('/help/*', (req, res) => {
+    res.render('help404', {
+        title: "Didn't find what you are looking for?",
+        authorName: 'Elad Ron'
+    });
+});
+
 app.get('/weather', (req, res)=>{
     res.send({
         "forcast": "sunny",
         "location": 34
     });
 });
+
+
+app.get('*', (req, res) => {
+    res.render('404', {
+        title: "About me",
+        authorName: 'Elad Ron'
+    });
+});
+
+
 
 app.listen(keys.expressPort, ()=>{
     console.log('Server is up on port '+ keys.expressPort);
