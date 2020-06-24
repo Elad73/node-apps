a88c8f74916c625a33f80729477e2d361e620490
EladRon <elad.ron.g@gmail.com>
2020-06-24 14:01:51 +0300

    adding fetch function to prepare fetching json data from client side

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,1 +1,12 @@
 console.log('Client side javascript code loaded');
+
+fetch('http://localhost:3000/weather?city=!').then((response) => {
+    response.json().then((data) => {
+        if (data.error) {
+            console.log(data.error);
+        } else {
+            console.log(data.location);
+            console.log(data.forcast);
+        }
+    })
+})
