8f59bd7d243584251cfb8ecd4ff0a4b2a6465d1e
EladRon <elad.ron.g@gmail.com>
2020-06-24 14:16:57 +0300

    adding form to the client side with js code to featch the weather forecast according to the form's input location

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,12 +1,21 @@
 console.log('Client side javascript code loaded');
 
-fetch('http://localhost:3000/weather?city=!').then((response) => {
-    response.json().then((data) => {
-        if (data.error) {
-            console.log(data.error);
-        } else {
-            console.log(data.location);
-            console.log(data.forcast);
-        }
+const weatherForm = document.querySelector('form');
+const search = document.querySelector('input');
+
+weatherForm.addEventListener('submit', (e)=> {
+    e.preventDefault();
+    
+    const location = search.value;
+
+    fetch('http://localhost:3000/weather?city=' + location).then((response) => {
+        response.json().then((data) => {
+            if (data.error) {
+                console.log(data.error);
+            } else {
+                console.log(data.location);
+                console.log(data.forcast);
+            }
+        })
     })
 })
