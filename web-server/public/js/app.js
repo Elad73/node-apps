64e45307c64a96541737c72942a9fe145748f83a
EladRon <elad.ron.g@gmail.com>
2020-06-24 15:35:46 +0300

    styling the weather forecast page

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -2,19 +2,28 @@ console.log('Client side javascript code loaded');
 
 const weatherForm = document.querySelector('form');
 const search = document.querySelector('input');
+const messageOne = document.querySelector('#message-1');
+const messageTwo = document.querySelector('#message-2');
 
 weatherForm.addEventListener('submit', (e)=> {
     e.preventDefault();
     
+    messageOne.textContent = 'Loading...';
+    messageTwo.textContent = '';
+
+ 
     const location = search.value;
 
     fetch('http://localhost:3000/weather?city=' + location).then((response) => {
         response.json().then((data) => {
             if (data.error) {
-                console.log(data.error);
+                messageOne.textContent = data.error;                
             } else {
-                console.log(data.location);
-                console.log(data.forcast);
+                messageOne.textContent = data.location;
+                
+                messageTwo.textContent += 'Temperature: ' + data.forcast.temperature + '; ';
+                messageTwo.textContent += 'Weather: ' + data.forcast.desc + '; ';
+                messageTwo.textContent += 'Feels like: ' + data.forcast.feelslike;
             }
         })
     })
