dc45c4ce096417b612ba07f3a7d9888e14199094
EladRon <elad.ron.g@gmail.com>
2020-06-26 12:09:25 +0300

    installing the mongoose library, adding db folder with the mongoose file

--- src/mongodb.js
+++ src/mongodb.js
@@ -1,7 +1,6 @@
 //CRUD create read update delete
 
-const mongodb = require('mongodb');
-const MongoClient = mongodb.MongoClient;
+const { MongoClient, ObjectID} = require('mongodb');
 
 const connectionURL = 'mongodb://127.0.0.1:27017';
 const databaseName = 'task-manager';
@@ -12,56 +11,5 @@ MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) =>
     }
 
     const db = client.db(databaseName);
-
-    // db.collection('users').insertOne({
-    //    name: 'Elad',
-    //    age: 46 
-    // }, (error, result)=>{
-    //     if (error) {
-    //         return console.log('Unable to insert user');
-    //     }
-
-    //     //ops is array of documents inserted
-    //     console.log(result.ops);
-    // });
-
-    // db.collection('users').insertMany([
-    //     {
-    //         name: 'Jen',
-    //         age: 38
-
-    //     }, {
-    //         name: 'Gunther',
-    //         age: 55
-    //     }
-    // ], (error, result)=> {
-    //     if (error) {
-    //         return console.log('Unable to insert many rows');
-    //     }
-
-    //     console.log(result.ops);
-
-    // });
-
-    const currentTasks = [
-        {
-            description: "Water the plants",
-            completed: false
-
-        }, {
-            description: "add new design system",
-            completed: false
-        }, {
-            description: "write a blessing letter to Libi",
-            completed: true
-        }
-    ];
-    db.collection('tasks').insertMany(currentTasks, (error, result)=>{
-        if (error) {
-            return console.log('Unable to add tasks to \'tasks\' collection');
-        }
-
-        console.log(result.ops);
-    });
 });
 
